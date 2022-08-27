3f4acf1ea37c7015e42bcbe72b1e2a4130386b4e
Saifeddine Bejaoui <6457481-Saifeddineb@users.noreply.gitlab.com>
2022-08-27 17:56:46 +0200

    add swaggers documentation

--- main.py
+++ main.py
@@ -1,22 +1,166 @@
+import jwt
+
 # Packages
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, APIRouter
+from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from tortoise.contrib.fastapi import register_tortoise
+from passlib.hash import bcrypt
+
+# Project models
+from models import Users, Users_Pydantic, UsersIn_Pydantic
 
 app = FastAPI()
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='token')
 
+JWT_SECRET = 'jpoja"%%6())iip"jçu%2é"!!ué"z4d484s686q46q4dzjziç"uçu'
+
+
+async def authenticate_user(username: str, password: str):
+    """ Authenticate a user with his credentials
+
+    Parameters
+    ----------
+    username : str, 
+        Account username used in registration form
+    password : str, 
+        Account password used in registration form
+
+    Returns
+    -------
+    Users instance, 
+        Users instance of authenticated user
+    """
+    user = await Users.get(username=username)
+    if not user:
+        return False
+    if not user.verify_password(password):
+        return False
+    return user
+
+
+@app.post('/token')
+async def generate_token(form_data: OAuth2PasswordRequestForm = Depends()):
+    """ Generate when user is authenticated
+
+    Parameters
+    ----------
+    form_data : OAuth2PasswordRequestForm, optional, 
+        Login Form that are used in this endpoint, by default Depends()
+
+    Returns
+    -------
+    string, 
+        User access token value and type
+    """
+    user = await authenticate_user(form_data.username, form_data.password)
+
+    if not user:
+        return {'error': 'invalid credentials'}
+    
+    # Create new user with credentials: username and hashed password
+    user_obj = await Users_Pydantic.from_tortoise_orm(user)
+
+    # To improve security: Remove password hash from the payload
+    # (modify user_obj.dict())
+    print(user_obj.dict().pop('password_hash'))
+    print('dict without password_hash', user_obj.dict())
+    token = jwt.encode(user_obj.dict().pop('password_hash'), JWT_SECRET)
+
+    return {'access_token': token, 'token_type': 'bearer' }
+
+
 @app.get("/")
 async def index(token: str = Depends(oauth2_scheme)):
+    """ API root path
+
+    Parameters
+    ----------
+    token : str, optional, 
+        User access token, by default Depends(oauth2_scheme)
+
+    Returns
+    -------
+    token, 
+        Encrypted access token with JWT_SECRET value
+    """
     return {'the_token': token}
 
-# Simple Authentification
-@app.post('/token')
-# Depends means return can be a form or nothing
-async def token(form_data: OAuth2PasswordRequestForm = Depends()):
-    return {'access_token': form_data.username + 'token'}
 
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    """ Get current user authenticated by decrypting the encrypted access token
+    
+    This function should be used in every endpoint that need authentification
+    control
+
+    Parameters
+    ----------
+    token : str, optional, 
+        Encrypted access token, by default Depends(oauth2_scheme)
+
+    Returns
+    -------
+    Users model instance, 
+        User model with attributes registered: id, username, password_hash
+
+    Raises
+    ------
+    HTTPException, 
+        If payload could not be decrypted successfully return 401 Unauthorized Error
+    """
+    try:
+        # decode the token
+        payload = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])
+        user = await Users.get(id=payload.get('id'))
+    
+    except Exception as exc:
+        print('Exception', exc)
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Invalid username or password'
+        )
+        
+    return await Users_Pydantic.from_tortoise_orm(user)
+    
+
+@app.post('/users', response_model=Users_Pydantic)
+async def create_user(user: UsersIn_Pydantic):
+    """ Create user with UserIn_Pydantic attributes
+
+    Parameters
+    ----------
+    user : UsersIn_Pydantic, 
+        User instance without read-only attributes
+
+    Returns
+    -------
+    Users instance, 
+        User instance with input credentials, username and hashed password
+    """
+    user_obj = Users(
+        username=user.username,
+        password_hash=bcrypt.hash(user.password_hash)
+    )
+    await user_obj.save()
+    return await Users_Pydantic.from_tortoise_orm(user_obj)
+
+
+@app.get('/users/me', response_model=Users_Pydantic)
+async def get_user(user: Users_Pydantic = Depends(get_current_user)):
+    """ Get user authenticated by calling get_current_user function
+
+    Parameters
+    ----------
+    user : Users_Pydantic, optional
+        User with username and hashed password, by default Depends(get_current_user)
+
+    Returns
+    -------
+    Users instance
+        User with username and hashed password registered in database
+    """
+    return user
 
 register_tortoise(
     app,
--- models.py
+++ models.py
@@ -1,9 +1,22 @@
 from tortoise.contrib.pydantic import pydantic_model_creator
 from tortoise import fields
 from tortoise.models import Model
+from passlib.hash import bcrypt
 
 
 class Users(Model):
+    """ Authentification user models with credentials
+
+    Parameters
+    ----------
+    Model : Tortoise base class model
+        Simplify ORM process
+
+    Returns
+    -------
+    Class method
+        Allow to get any user registered based on his username
+    """
     id = fields.IntField(pk=True)
     username = fields.CharField(50)
     password_hash = fields.CharField(128)
@@ -13,5 +26,13 @@ class Users(Model):
         return cls.get(username=username)
 
     def verify_password(self, password):
-        return True
+        return bcrypt.verify(password, self.password_hash)
 
+
+# Custom data types that represents what users have
+Users_Pydantic = pydantic_model_creator(Users, name='User')
+
+# Custom data types that represents what users can pass as input
+UsersIn_Pydantic = pydantic_model_creator(
+    Users, name='UserIn', exclude_readonly=True
+)
