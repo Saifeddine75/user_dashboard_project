17916853564ecc3c4116bda7434637629156ba19
Saifeddine Bejaoui <6457481-Saifeddineb@users.noreply.gitlab.com>
2022-09-01 21:09:43 +0200

    only keep function without api routes decorator

--- webapps/authentication/utils/auth_tools.py
+++ webapps/authentication/utils/auth_tools.py
@@ -13,8 +13,6 @@ from webapps.authentication.models import Users, Users_Pydantic, UsersIn_Pydanti
 from settings import oauth2_scheme, JWT_SECRET_KEY
 
 
-
-### AUTHENTICATE USER THROUGH HASH DECRYPT
 async def authenticate_user(username: str, password: str):
     """ Authenticate a user with his username and his hash password decrypted
 
@@ -64,6 +62,9 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
         payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=['HS256'])
         user = await Users.get(id=payload.get('id'))
 
+        if user.username is not None:
+            token_data = TokenData(username=username)
+        
     except Exception as exc:
         print('Exception', exc)
         raise HTTPException(
@@ -79,43 +80,3 @@ async def get_current_active_user(current_user: Users_Pydantic = Depends(get_cur
         raise HTTPException(status_code=400, detail="Inactive user")
     return current_user
 
-
-async def generate_token(username: str, password: str):
-    # async def generate_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    """ Generate when user is authenticated
-
-    Parameters
-    ----------
-    form_data : OAuth2PasswordRequestForm, optional, 
-        Login Form that are used in this endpoint, by default Depends()
-
-    Returns
-    -------
-    string, 
-        User access token value and type
-    """
-    try:
-        print('form', username, password)
-        user = await authenticate_user(username, password)
-    except Exception as exc:
-        print('exception', exc)
-        return {'error': 'failed to authenticated user'}
-        # raise HTTPException(
-        #     status_code=status.HTTP_404_NOT_FOUND,
-        #     detail='User not found'
-        # )
-
-    # Create new user with credentials: username and hashed password
-    user_obj = await Users_Pydantic.from_tortoise_orm(user)
-
-    # *** To improve security: Don't show password hash in the payload
-    # Hash it another time to produce a secure access token
-    user_obj_safe = {}
-    user_obj_safe = user_obj.dict()
-    for _ in range(10):
-        user_obj_safe['password_hash'] = bcrypt.hash(
-            user_obj_safe['password_hash'])
-        print(user_obj_safe['password_hash'])
-    token = jwt.encode(user_obj_safe, JWT_SECRET_KEY)
-
-    return {'access_token': token, 'token_type': 'bearer'}
